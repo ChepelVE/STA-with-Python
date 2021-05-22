ae06526df1ae6a26e538f1a6c28be7dc42d7249f
ViktorChepelTR <63342270+ViktorChepelTR@users.noreply.github.com>
2021-05-22 13:11:40 +0300

    task1 changes
    - some tests added
    - '\n' was cutted from the value
    - str to int was changed

--- Lecture_8_Object_Model_homework/task1.py
+++ Lecture_8_Object_Model_homework/task1.py
@@ -7,11 +7,15 @@ class KeyValueStorage:
     def __init__(self, filename: str):
         with open(filename) as pairs:
             for pair in pairs:
+                pair = pair.strip("\n")
                 key, value = pair.split("=")
-                self.d[key] = value
+                try:
+                    self.d[key] = int(value)
+                except ValueError:
+                    self.d[key] = value
                 if key.isidentifier():
                     if key not in dir(builtins):
-                        setattr(self, key, value)
+                        setattr(self, key, self.d[key])
                 else:
                     raise ValueError(f"{key} is incorrect identifier!")
 
@@ -19,13 +23,5 @@ class KeyValueStorage:
         self.d[key] = item
 
     def __getitem__(self, item):
-        if isinstance(self.d[item], int):
-            return int(self.d[item])
         return self.d[item]
 
-
-if __name__ == '__main__':
-    storage = KeyValueStorage('task1.txt')
-    print(storage['name'])  # will be string 'kek'
-    print(storage.song)   # will be 'shadilay'
-    print(storage.power)  # will be integer 9001
